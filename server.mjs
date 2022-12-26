ea3242610edb3decbd83afe6201c69cec7bb8198
Saaim-K <saaimkhan221@gmail.com>
2022-12-26 19:41:43 +0500

    Login Signup Completed

--- server.mjs
+++ server.mjs
@@ -4,6 +4,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import jwt from 'jsonwebtoken'
 import cookieParser from 'cookie-parser';
+import { stringToHash, varifyHash } from "bcrypt-inzi"
 
 
 
@@ -14,37 +15,41 @@ const SECRET = process.env.SECRET || "topsecret";
 
 
 
-
 app.use(express.json());
 mongoose.connect(mongodbURI)
-app.use(cookieParser());
-
-
-
+app.use(cookieParser())
 app.use(cors({
     origin: ['http://localhost:3000', "*"],
     credentials: true
 }));
 
 
+
 // ----------------------------------- MongoDB -----------------------------------
-let usersSchema = new mongoose.Schema({
+let productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: Number,
+    ratings: Number,
+    description: String,
+    createdOn: { type: Date, default: Date.now }
+})
+const productModel = mongoose.model('Products', productSchema);
+
+const userSchema = new mongoose.Schema({
     firstName: { type: String },
     lastName: { type: String },
     email: { type: String, required: true },
     password: { type: String, required: true },
     createdOn: { type: Date, default: Date.now },
-})
-const userModel = mongoose.model('Users', usersSchema);
+});
+const userModel = mongoose.model('Users', userSchema);
 // ----------------------------------- MongoDB -----------------------------------
 
 
 
-// ----------------------------------- SignUp -----------------------------------
-pp.post("/signup", (req, res) => {
-
+// ----------------------------------- Signup using JSON Web Token -----------------------------------
+app.post("/signup", (req, res) => {
     let body = req.body;
-
     if (!body.firstName || !body.lastName || !body.email || !body.password) {
         res.status(400).send(
             `required fields missing, request example: 
@@ -57,24 +62,23 @@ pp.post("/signup", (req, res) => {
         );
         return;
     }
-
     req.body.email = req.body.email.toLowerCase();
-
-    // check if user already exist // query email user
     userModel.findOne({ email: body.email }, (err, user) => {
         if (!err) {
-            console.log("user: ", user);
-
-            if (user) { // user already exist
-                console.log("user already exist: ", user);
-                res.status(400).send({ message: "user already exist,, please try a different email" });
+            console.log("User: ", user);
+            // ------------------------ Check if user already exists ------------------------
+            if (user) {
+                console.log("User already exist: ", user);
+                res.status(400).send({
+                    message: "User already exists. Please try a different email"
+                });
                 return;
+            }
+            // ------------------------ Check if user already exists ------------------------
 
-            } else { // user not already exist
-
-                // bcrypt hash
+            // ------------------------ Check if user doesn't exists and Create a User ------------------------
+            else {
                 stringToHash(body.password).then(hashString => {
-
                     userModel.create({
                         firstName: body.firstName,
                         lastName: body.lastName,
@@ -84,33 +88,149 @@ pp.post("/signup", (req, res) => {
                         (err, result) => {
                             if (!err) {
                                 console.log("data saved: ", result);
-                                res.status(201).send({ message: "user is created" });
+                                res.status(201).send(
+                                    {
+                                        message: "User created"
+                                    });
                             } else {
                                 console.log("db error: ", err);
-                                res.status(500).send({ message: "internal server error" });
+                                res.status(500).send(
+                                    {
+                                        message: "Internal Server Error"
+                                    });
                             }
                         });
                 })
-
             }
+            // ------------------------ Check if user doesn't exists and Create a User ------------------------
         } else {
-            console.log("db error: ", err);
-            res.status(500).send({ message: "db error in query" });
+            console.log("database error: ", err);
+            res.status(500).send({ message: "database error in query" });
             return;
         }
     })
 });
-// ----------------------------------- SignUp -----------------------------------
+// ----------------------------------- Signup using JSON Web Token -----------------------------------
 
+// ----------------------------------- Login using JSON Web Token -----------------------------------
+app.post("/login", (req, res) => {
 
+    let body = req.body;
+    body.email = body.email.toLowerCase();
 
+    if (!body.email || !body.password) {
+        res.status(400).send(
+            `required fields missing, request example: 
+                {
+                    "email": "abc@abc.com",
+                    "password": "12345"
+                }`
+        );
+        return;
+    }
 
+    // ------------------------ Check if user exists ------------------------
+    userModel.findOne(
+        { email: body.email },
+        // ---------- Projection in MongoDB ----------
+        // ----- 1st Method -----
+        // { firstName: 1, lastName: 1, email: 1, password: 0 },//1= include ,0 = exclude 
+        // ----- 1st Method -----
+        // ----- 2nd Method -----
+        "firstName lastName email password",//  - (minus sign) = exclude 
+        // ----- 2nd Method -----
+        // ---------- Projection in MongoDB ----------
+        (err, data) => {
+            if (!err) {
+                console.log("data: ", data);
+                if (data) {
+                    varifyHash(body.password, data.password).then(isMatched => {
+                        console.log("isMatched: ", isMatched);
+                        if (isMatched) {
+                            const token = jwt.sign({
+                                _id: data._id,
+                                email: data.email,
+                                iat: Math.floor(Date.now() / 1000) - 30,
+                                exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+                            }, SECRET);
+                            console.log("token: ", token);
+                            res.cookie('Token', token, {
+                                maxAge: 86_400_000,
+                                httpOnly: true
+                            });
+                            res.send({
+                                message: "login successful",
+                                profile: {
+                                    email: data.email,
+                                    firstName: data.firstName,
+                                    lastName: data.lastName,
+                                    age: data.age,
+                                    _id: data._id
+                                }
+                            });
+                            return;
+                        } else {
+                            console.log("password did not match");
+                            res.status(401).send({ message: "Incorrect email or password" });
+                            return;
+                        }
+                    })
+                } else { // user not already exist
+                    console.log("user not found");
+                    res.status(401).send({ message: "Incorrect email or password" });
+                    return;
+                }
+            } else {
+                console.log("db error: ", err);
+                res.status(500).send({ message: "login failed, please try later" });
+                return;
+            }
+        })
+    // ------------------------ Check if user exists ------------------------
+})
+// ----------------------------------- Login using JSON Web Token -----------------------------------
 
+// ----------------------------------- Logout using JSON Web Token -----------------------------------
+app.post("/logout", (req, res) => {
+    res.cookie('Token', '', {
+        maxAge: 1,
+        httpOnly: true
+    });
+    res.send({ message: "Logout successful" });
+})
+// ----------------------------------- Logout using JSON Web Token -----------------------------------
 
-
-
-
-
+// ----------------------------------- Middleware JSON Web Token -----------------------------------
+app.use((req, res, next) => {
+    console.log("req.cookies: ", req.cookies);
+    if (!req?.cookies?.Token) {
+        res.status(401).send({
+            message: "include http-only credentials with every request"
+        })
+        return;
+    }
+    jwt.verify(req.cookies.Token, SECRET, function (err, decodedData) {
+        if (!err) {
+            console.log("decodedData: ", decodedData);
+            const nowDate = new Date().getTime() / 1000;
+            if (decodedData.exp < nowDate) {
+                res.status(401);
+                res.cookie('Token', '', {
+                    maxAge: 1,
+                    httpOnly: true
+                });
+                res.send({ message: "token expired" })
+            } else {
+                console.log("token approved");
+                req.body.token = decodedData
+                next();
+            }
+        } else {
+            res.status(401).send("invalid token")
+        }
+    });
+})
+// ----------------------------------- Middleware JSON Web Token -----------------------------------
 
 
 // ----------------------------------- Create/Add Product -----------------------------------
@@ -122,7 +242,7 @@ app.post('/product', (req, res) => {
         })
         return;
     }
-    userModel.create({
+    productModel.create({
         name: body.name,
         price: body.price,
         ratings: body.ratings,
@@ -149,7 +269,7 @@ app.post('/product', (req, res) => {
 // ----------------------------------- Get Product -----------------------------------
 // ------------------------ Get All Product ------------------------
 app.get('/products', (req, res) => {
-    userModel.find({}, (error, allFound) => {
+    productModel.find({}, (error, allFound) => {
         if (!error) {
             console.log("uploaded", allFound)
             res.send({
@@ -169,7 +289,7 @@ app.get('/products', (req, res) => {
 // ------------------------ Get Specified Product ------------------------
 app.get('/product/:id', (req, res) => {
     const id = req.params.id
-    userModel.findOne({ _id: id }, (error, found) => {
+    productModel.findOne({ _id: id }, (error, found) => {
         if (!error) {
             if (found) {
                 res.send({
@@ -198,7 +318,7 @@ app.get('/product/:id', (req, res) => {
 // ----------------------------------- Delete Product -----------------------------------
 // ------------------------ Delete All Product ------------------------
 app.delete('/products', (req, res) => {
-    userModel.deleteMany({}, (error, data) => {
+    productModel.deleteMany({}, (error, data) => {
         if (!error) {
             res.send({
                 message: `All products deleted`
@@ -215,7 +335,7 @@ app.delete('/products', (req, res) => {
 // ------------------------ Delete Specified Product ------------------------
 app.delete('/product/:id', (req, res) => {
     const id = req.params.id
-    userModel.deleteOne({ _id: id }, (error, deletedData) => {
+    productModel.deleteOne({ _id: id }, (error, deletedData) => {
         console.log(deletedData)
         if (!error) {
             if (deletedData.deletedCount === 1) {
@@ -255,7 +375,7 @@ app.put('/product/:id', async (req, res) => {
         return;
     }
     try {
-        let data = await userModel.findByIdAndUpdate(id,
+        let data = await productModel.findByIdAndUpdate(id,
             {
                 name: body.name,
                 price: body.price,
